8ec73ac5c77c7ceeca96327ae972a229b1dafc58
Matthew Davis <matthew@matthewdavis.io>
2018-10-31 23:12:29 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+
+        AppComponent,
+        HomeComponent,
+        ContactComponent
+
+    ],
+    imports: [
+
+        BrowserModule,
+
+        MatToolbarModule,
+        MatButtonModule,
+
+        RouterModule.forRoot([
+
+            {
+
+                path: 'home',
+                component: HomeComponent
+
+            }, {
+
+                path: 'contact',
+                component: ContactComponent
+
+            }
+
+        ])
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
